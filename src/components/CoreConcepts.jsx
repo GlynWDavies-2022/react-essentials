9f05e5977b30074681d1b3ac2f259c3787ae8318
Glyn Davies <glynwdavies@gmail.com>
2026-04-18 07:35:38 +0100

    Refactored.

--- src/components/CoreConcepts.jsx
+++ src/components/CoreConcepts.jsx
@@ -5,17 +5,17 @@ import { CORE_CONCEPTS } from '../data/data';
 function CoreConcepts() {
     return (
         <section id="core-concepts">
-        <h2>Core Concepts</h2>
-        <ul>
-            {CORE_CONCEPTS.map((concept) => (
-              <CoreConcept
-                key={concept.title}
-                title={concept.title}
-                description={concept.description}
-                img={concept.image}
-              />
-            ))}
-        </ul>
+          <h2>Core Concepts</h2>
+          <ul>
+              {CORE_CONCEPTS.map((concept) => (
+                <CoreConcept
+                  key={concept.title}
+                  title={concept.title}
+                  description={concept.description}
+                  img={concept.image}
+                />
+              ))}
+          </ul>
         </section>
     )
 }
